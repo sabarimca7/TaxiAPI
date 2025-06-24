faf00504dc59b84ca7a07cd69f8ad57162ebc0d4
sabarimalai_alagar <sabarimalai_alagar>
2025-06-24 21:16:28 +0530

    updates

--- TaxiAPI/Controllers/BookingController.cs
+++ TaxiAPI/Controllers/BookingController.cs
@@ -1,23 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TaxiApplication.Server.Request;
 
 namespace TaxiApplication.Server.Controllers;
+
 
 [ApiController]
 [Route("api/[controller]")]
 public class BookingController : ControllerBase
 {
     private readonly ApplicationDbContext _context;
-    public BookingController(ApplicationDbContext context) => _context = context;
+    public BookingController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public async Task<IActionResult> CreateBooking([FromBody] CabBooking bookingRequest)
     {
-        var cab = await _context.Cab.FindAsync(bookingRequest.CabId);
-        if (cab == null || !cab.IsAvailable)
-            return BadRequest("Cab not available");
+        //var cab = await _context.Cab.FindAsync(bookingRequest.CabId);
+        //if (cab == null || !cab.IsAvailable)
+        //    return BadRequest("Cab not available");
 
-        cab.IsAvailable = false;
+        //cab.IsAvailable = false;
         Booking booking = new Booking()
         {
             CabId = bookingRequest.CabId,
@@ -32,5 +37,64 @@ public class BookingController : ControllerBase
         _context.Booking.Add(booking);
         await _context.SaveChangesAsync();
         return Ok(booking);
+        Console.WriteLine("Booking received");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetBookingList()
+    {
+        var BookingList = await _context.Booking
+            .ToListAsync();
+
+        return Ok(BookingList); // <-- This sends data to Angular
+    }
+
+    [HttpGet("id")]
+    public async Task<IActionResult> GetBookingListById(int id)
+    {
+        var User = await _context.Booking.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+        if (User == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(User);
+    }
+
+    [HttpPut("id")]
+    public async Task<IActionResult> UpdateBooking(int id, Booking UserRequest)
+    {
+        var User = await _context.Booking.Where(x => x.Id == id).FirstOrDefaultAsync();
+        if (User == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            User.CabId = UserRequest.CabId;
+            User.FromLocation = UserRequest.FromLocation;
+            User.ToLocation = UserRequest.ToLocation;
+            User.PickupDateTime = UserRequest.PickupDateTime;
+            User.ReturnDateTime = UserRequest.ReturnDateTime;
+            User.CustomerName = UserRequest.CustomerName;
+            User.PhoneNumber = UserRequest.PhoneNumber;
+            User.Address = UserRequest.Address;
+        }
+
+        await _context.SaveChangesAsync();
+
+        return Ok(User);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _context.Booking.FirstOrDefaultAsync(x => x.Id == id);
+        if (user == null)
+            return NotFound();
+
+        _context.Booking.Remove(user);
+        await _context.SaveChangesAsync();
+        return Ok(user);
     }
 }
